052fa748166725c56a17c4db5699bf85dafdda59
Rakesh <kexar007@gmail.com>
2023-10-25 20:46:58 +0530

    add base files

---
+++ script.js
@@ -0,0 +1,8 @@
+const main = document.querySelector('#main');
+const canvas = document.querySelector('#canvas')
+
+
+
+const div = document.createElement('div');
+div.classList.add('tile');  
+canvas.appendChild(div);
