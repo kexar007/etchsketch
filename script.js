db239a8f4a5f9a6abc295bb0dcf8d0b57cf724ec
Rakesh R <kexar007@gmail.com>
2023-10-26 23:11:52 +0530

    add tiles in loop

--- script.js
+++ script.js
@@ -3,6 +3,19 @@ const canvas = document.querySelector('#canvas')
 
 
 
-const div = document.createElement('div');
-div.classList.add('tile');  
-canvas.appendChild(div);
+//const div = document.createElement('div');
+//div.classList.add('tile');  
+//canvas.appendChild(div);
+
+for (let i = 0; i < 16; i++) {
+    
+    const div = document.createElement('div');
+    div.classList.add('tile'); 
+    div.innerHTML="<h2>"+i+"<h2>" ;
+    canvas.appendChild(div);
+
+
+
+
+    
+}
